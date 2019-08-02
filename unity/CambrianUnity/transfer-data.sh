1eeb7c46ab92bf7983e179278b396d0cee1969fb
Joel Teply <joel@cambriantech.com>
2019-08-02 14:21:10 -0500

    build dir

--- unity/CambrianUnity/transfer-data.sh
+++ unity/CambrianUnity/transfer-data.sh
@@ -9,6 +9,8 @@ fi
 SRC_PROJECT_ROOT="${CBROOT}/unity/Build/ios"
 INSTALL_DIR="${CBROOT}/prebuilts/unity"
 
+mkdir -p "${INSTALL_DIR}"
+
 #transfer Data directory
 DATA_SRC="${SRC_PROJECT_ROOT}/Data"
 DATA_DEST="${INSTALL_DIR}/Data"
