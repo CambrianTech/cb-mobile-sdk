26dfb3fe8c17ab04a8699678ee4574b3909e24a4
Joel Teply <joel@cambriantech.com>
2019-08-02 13:30:26 -0500

    fastcv

---
+++ prebuilts/fastcv/samples/fastcorner/jni/Application.mk
@@ -0,0 +1,10 @@
+
+
+#comment out for regular Cambrian SDK
+APP_PLATFORM := android-9
+
+FAST_CV_PATH := $(CB)/prebuilts/fastcv
+
+APP_STL := gnustl_static
+APP_ABI := armeabi-v7a arm64-v8a
+NDK_TOOLCHAIN_VERSION := clang
---
+++ prebuilts/fastcv/samples/fastcvdemo/jni/Application.mk
@@ -0,0 +1,8 @@
+
+
+#comment out for regular Cambrian SDK
+APP_PLATFORM := android-19
+
+APP_STL := gnustl_static
+APP_ABI := armeabi-v7a arm64-v8a
+NDK_TOOLCHAIN_VERSION := clang
