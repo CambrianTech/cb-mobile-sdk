bb1116004a1004c628bce22fa94efd2749b63d47
Joel Teply <joel@cambriantech.com>
2019-12-12 16:39:12 -0600

    pissed off

--- ios/ShawVRAR/ShawARVR/ShawARVR/DB/DataSource.swift
+++ ios/ShawVRAR/ShawARVR/ShawARVR/DB/DataSource.swift
@@ -12,11 +12,10 @@ class DataSource {
     
     let jsonPath = "datasource.json"
     let webSource:String
-    
-    let test = "https://shawfloors.com/api/odata/Hardwoods?$top=1000&$skip=0&$orderby=StyleSequence,UniqueId&$count=true&$select=UniqueId,SellingStyleNbr,SellingColorNbr,SellingStyleName,SellingColorName,StaticRoomFlag,Vignette,ColorCount,MSRPRange,HasSwatchImage,SampleCount,CollectionDesc,ColorFamilyDesc,Shade,SurfaceTextureShortDesc,InstallationType,CollectionDesc&$filter=(IsDropped%20eq%20false)%20and%20(ColorCount%20gt%200)%20and%20(ProductGroupPermanentName%20eq%20%27shawfloors%27)%20and%20(ProductGroupShowOnVizTool%20eq%20true)%20and%20(HasMainImage%20eq%20true)%20and%20(StaticRoomFlag%20eq%20true%20or%20HasRenderImage%20eq%20true)%20and%20(IsDefaultStyleColor%20eq%20true)"
+    let productGroup = "shawfloors"
     
     var topLevelCategories: [ProductCategory] = []
-    var jsonCategories:Array<AnyObject>
+    var jsonCategories:Dictionary<String, Dictionary<String, String>> = Dictionary<String, Dictionary<String, String>>()
     
     private init() {
         if let path = Bundle.main.path(forResource: jsonPath, ofType: nil) {
@@ -33,7 +32,10 @@ class DataSource {
                 }
                 
                 self.webSource = source
-                self.jsonCategories = categories
+                for categoryJson in categories {
+                    let category = categoryJson as! Dictionary<String, String>
+                    jsonCategories[category["name"]!] = category
+                }
                 self.topLevelCategories = parseCategories(categoriesJSON: categories)
                 return
             }
@@ -75,6 +77,36 @@ class DataSource {
         category.name = parsed["displayName"] as! String
         category.thumbnailPath = Bundle.main.url(forResource: parsed["thumbnailPath"] as? String, withExtension: nil)
         
+        requestCategoryData(category.code)
+        
         return category
+    }
+    
+     let test = "https://shawfloors.com/api/odata/Laminates?$top=1000&$skip=0&$orderby=StyleSequence,UniqueId&$count=true&$select=UniqueId,SellingStyleNbr,SellingColorNbr,SellingStyleName,SellingColorName,StaticRoomFlag,Vignette,ColorCount,MSRPRange,HasSwatchImage,SampleCount,CollectionDesc,ColorFamilyDesc,CollectionDesc&$filter=(IsDropped eq false) and (ColorCount gt 0) and (ProductGroupPermanentName eq %27shawfloors%27) and (ProductGroupShowOnVizTool eq true) and (HasMainImage eq true) and (StaticRoomFlag eq true or HasRenderImage eq true) and (IsDefaultStyleColor eq true)"
+    
+    
+    let pageSize = 1000
+    
+    private func encodeUrl(_ string:String) -> String {
+        return string.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)!
+    }
+    
+    private func requestCategoryData(_ categoryCode:String, page:Int=0) {
+        
+        let categoryData = jsonCategories[categoryCode]!
+        
+        let orderBy = "StyleSequence,UniqueId&$count=true"
+        var select = "UniqueId,SellingStyleNbr,SellingColorNbr,SellingStyleName,SellingColorName,StaticRoomFlag,Vignette,ColorCount,MSRPRange,HasSwatchImage,SampleCount"
+        select += "," + categoryData["select"]!
+        
+        let filter = "(IsDropped eq false) and (ColorCount gt 0) and (ProductGroupPermanentName eq 'shawfloors') and (ProductGroupShowOnVizTool eq true) and (HasMainImage eq true) and (StaticRoomFlag eq true or HasRenderImage eq true) and (IsDuplicate eq false) and (IsDefaultStyleColor eq true) and (ProductCode ne '22')"
+        
+        var urlString = "\(self.webSource)/\(categoryData["source"]!)?$top=\(pageSize)&$skip=\(page * pageSize)"
+        
+        urlString += "&$orderby=\(encodeUrl(orderBy))"
+        urlString += "&$select=\(encodeUrl(select))"
+        urlString += "&$filter=\(encodeUrl(filter))"
+        
+        print(urlString)
     }
 }
