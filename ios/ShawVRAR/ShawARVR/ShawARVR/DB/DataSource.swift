290c7ceea1e01f922f47ec5999a37d951fa9a229
Joel Teply <joel@cambriantech.com>
2019-12-12 16:53:04 -0600

    fixed query

--- ios/ShawVRAR/ShawARVR/ShawARVR/DB/DataSource.swift
+++ ios/ShawVRAR/ShawARVR/ShawARVR/DB/DataSource.swift
@@ -99,7 +99,8 @@ class DataSource {
         var select = "UniqueId,SellingStyleNbr,SellingColorNbr,SellingStyleName,SellingColorName,StaticRoomFlag,Vignette,ColorCount,MSRPRange,HasSwatchImage,SampleCount"
         select += "," + categoryData["select"]!
         
-        let filter = "(IsDropped eq false) and (ColorCount gt 0) and (ProductGroupPermanentName eq '\(self.productGroup)') and (ProductGroupShowOnVizTool eq true) and (HasMainImage eq true) and (StaticRoomFlag eq true or HasRenderImage eq true) and (IsDuplicate eq false) and (IsDefaultStyleColor eq true) and (ProductCode ne '22')"
+        var filter = "(IsDropped eq false) and (ColorCount gt 0) and (ProductGroupPermanentName eq '\(self.productGroup)') and (ProductGroupShowOnVizTool eq true) and (HasMainImage eq true)"
+        filter += " and " + categoryData["categories"]!
         
         var urlString = "\(self.webSource)/\(categoryData["source"]!)?$top=\(pageSize)&$skip=\(page * pageSize)"
         
