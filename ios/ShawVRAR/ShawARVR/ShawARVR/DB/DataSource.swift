54902eff627c1f70f7864d3887b2ae91f5c0c82d
Joel Teply <joel@cambriantech.com>
2019-12-12 10:33:42 -0600

    datasource

--- ios/ShawVRAR/ShawARVR/ShawARVR/DB/DataSource.swift
+++ ios/ShawVRAR/ShawARVR/ShawARVR/DB/DataSource.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class DataSource {
     
+    let webSource = "https://shawfloors.com/api"
+    
+    let test = "https://shawfloors.com/api/odata/Hardwoods?$top=1000&$skip=0&$orderby=StyleSequence,UniqueId&$count=true&$select=UniqueId,SellingStyleNbr,SellingColorNbr,SellingStyleName,SellingColorName,StaticRoomFlag,Vignette,ColorCount,MSRPRange,HasSwatchImage,SampleCount,CollectionDesc,ColorFamilyDesc,Shade,SurfaceTextureShortDesc,InstallationType,CollectionDesc&$filter=(IsDropped%20eq%20false)%20and%20(ColorCount%20gt%200)%20and%20(ProductGroupPermanentName%20eq%20%27shawfloors%27)%20and%20(ProductGroupShowOnVizTool%20eq%20true)%20and%20(HasMainImage%20eq%20true)%20and%20(StaticRoomFlag%20eq%20true%20or%20HasRenderImage%20eq%20true)%20and%20(IsDefaultStyleColor%20eq%20true)"
+    
     private init() {
         
     }
@@ -26,4 +30,6 @@ class DataSource {
             return datasource
         }
     }
+    
+    
 }
