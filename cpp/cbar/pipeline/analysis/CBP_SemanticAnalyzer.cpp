8208f26c8fb624ebc821b57574cecea0972406d2
Joel Teply <joel@cambriantech.com>
2019-12-17 11:33:24 -0600

    do not use nornals at all

--- cpp/cbar/pipeline/analysis/CBP_SemanticAnalyzer.cpp
+++ cpp/cbar/pipeline/analysis/CBP_SemanticAnalyzer.cpp
@@ -79,14 +79,6 @@ namespace cbpipe {
             bool success = inferenceSemantic(rgb, segmentedImage);
             
             if (!success) return false;
-            
-            cv::Mat normalsResult;
-            success = inferenceNormals(rgb, normalsResult, tracker, frame);
-            if (!success) return false;
-            
-            deep_result normalsData;
-            normalsData.frame = frame;
-            normalsData.outputs.push_back(normalsResult);
 
             deep_result semanticData;
             semanticData.frame = frame;
@@ -105,7 +97,7 @@ namespace cbpipe {
             segmentedImage.setTo(0, mask == 0);
             
             cv::Mat groundMask, wallMask;
-            refineResults(rgb, normalsResult, segmentedImage, groundMask, wallMask);
+            refineResults(rgb, segmentedImage, groundMask, wallMask);
 
             semanticData.outputs[result_index_semantic_ground] = segmentedImage;
             semanticData.outputs[result_index_semantic_ground].setTo(0, groundMask > 0);
@@ -123,9 +115,8 @@ namespace cbpipe {
             }
             
             for (auto surfaceAnalyzer : renderer->getAnalyzersOfType<CBP_SurfaceAnalyzer>()) {
-                std::thread([surfaceAnalyzer, semanticData, normalsData](){
+                std::thread([surfaceAnalyzer, semanticData](){
                     surfaceAnalyzer->semanticDataUpdated(semanticData);
-                    surfaceAnalyzer->normalsDataUpdated(normalsData);
                 }).detach();
             }
             
@@ -133,7 +124,7 @@ namespace cbpipe {
                 CBP_WallFinder::semantic_data semantic;
                 semantic.frameIndex = frame->frameIndex;
                 semantic.rgb = rgb;
-                semantic.normals = normalsResult;
+                //semantic.normals = normalsResult;
                 semantic.semantic = semanticData.outputs[result_index_semantic_walls];
                 std::thread([wallFinder, semantic](){
                     wallFinder->semanticDataUpdated(semantic);
@@ -186,7 +177,7 @@ namespace cbpipe {
             return true;
         }
         
-        void refineResults(const cv::Mat &rgb, const cv::Mat &normals, const cv::Mat &semantic, cv::Mat &nonFloorMask, cv::Mat &nonWallMask) {
+        void refineResults(const cv::Mat &rgb, const cv::Mat &semantic, cv::Mat &nonFloorMask, cv::Mat &nonWallMask) {
             nonFloorMask = cv::Mat::zeros(rgb.rows, rgb.cols, CV_8UC1);
             nonWallMask = cv::Mat::zeros(rgb.rows, rgb.cols, CV_8UC1);
             
@@ -196,58 +187,49 @@ namespace cbpipe {
             Accelerated::roughErode(nonFloorMask, nonFloorMask, cv::Size(30,30));
             Accelerated::roughErode(nonWallMask, nonWallMask, cv::Size(30,30));
             
-            cv::Size sampleSize = cv::Size(10,10);
-            Eigen::Vector3f up(0,1,0);
+            //cv::Size sampleSize = cv::Size(10,10);
+            //Eigen::Vector3f up(0,1,0);
             
-#if DEBUG_NORMALS
-            cv::Mat normalsDebug = normals.clone();
-#endif
-            float nonFloorAngleThreshold = M_PI_2 * 0.7;
-            float nonWallAngleThreshold = M_PI_2 * 0.95;
-            
-            cv::Rect roi = cv::Rect(0,0, sampleSize.width, sampleSize.height);
-            for (roi.y=0; roi.y<rgb.rows; roi.y+=sampleSize.height) {
-                for (roi.x=0; roi.x<rgb.cols; roi.x+=sampleSize.width) {
-                    cv::Scalar mean, stddev;
-                    
-                    cv::Rect adjustedROI = roi;
-                    adjustedROI.width = fmin(roi.width, normals.cols - roi.x);
-                    adjustedROI.height = fmin(roi.height, normals.rows - roi.y);
-                    
-                    cv::meanStdDev(normals(adjustedROI), mean, stddev);
-                    
-                    auto direction = CBP_AnalysisUtil::colorToDirection(mean);
-                    auto angle = CBP_MatrixUtil::angleBetweenVectors(direction, up);
-                    
-                    int objectClass = 0;
-
-                    if (angle > nonFloorAngleThreshold) {
-                        objectClass |= 1;
-                        cv::rectangle(nonFloorMask, adjustedROI, cv::Scalar::all(255), CV_FILLED);
-                    }
-                    
-                    if (angle < nonWallAngleThreshold) {
-                        objectClass |= 2;
-                        cv::rectangle(nonWallMask, adjustedROI, cv::Scalar::all(255), CV_FILLED);
-                    }
-                    
-//                    if (stddev[1] < 1.0) {
-//                        if (angle < 0.1) {
-//                            //normals says floor
-//                            nonFloorMask(adjustedROI) = 0;
-//                        } else if (angle > nonWallAngleThreshold) {
-//                            //normals says wall
-//                            nonWallMask(adjustedROI) = 0;
-//                        }
+//#if DEBUG_NORMALS
+//            cv::Mat normalsDebug = normals.clone();
+//#endif
+//            float nonFloorAngleThreshold = M_PI_2 * 0.7;
+//            float nonWallAngleThreshold = M_PI_2 * 0.95;
+//
+//            cv::Rect roi = cv::Rect(0,0, sampleSize.width, sampleSize.height);
+//            for (roi.y=0; roi.y<rgb.rows; roi.y+=sampleSize.height) {
+//                for (roi.x=0; roi.x<rgb.cols; roi.x+=sampleSize.width) {
+//                    cv::Scalar mean, stddev;
+//
+//                    cv::Rect adjustedROI = roi;
+//                    adjustedROI.width = fmin(roi.width, normals.cols - roi.x);
+//                    adjustedROI.height = fmin(roi.height, normals.rows - roi.y);
+//
+//                    cv::meanStdDev(normals(adjustedROI), mean, stddev);
+//
+//                    auto direction = CBP_AnalysisUtil::colorToDirection(mean);
+//                    auto angle = CBP_MatrixUtil::angleBetweenVectors(direction, up);
+//
+//                    int objectClass = 0;
+//
+//                    if (angle > nonFloorAngleThreshold) {
+//                        objectClass |= 1;
+//                        cv::rectangle(nonFloorMask, adjustedROI, cv::Scalar::all(255), CV_FILLED);
 //                    }
-                    
-#if DEBUG_NORMALS
-                    if (objectClass > 0) {
-                        cv::rectangle(normalsDebug, adjustedROI, cv::Scalar(255 * (objectClass & 1), 255 * (objectClass & 2), 0));
-                    }
-#endif
-                    
-                }
+//
+//                    if (angle < nonWallAngleThreshold) {
+//                        objectClass |= 2;
+//                        cv::rectangle(nonWallMask, adjustedROI, cv::Scalar::all(255), CV_FILLED);
+//                    }
+//
+//#if DEBUG_NORMALS
+//                    if (objectClass > 0) {
+//                        cv::rectangle(normalsDebug, adjustedROI, cv::Scalar(255 * (objectClass & 1), 255 * (objectClass & 2), 0));
+//                    }
+//#endif
+//
+//                }
+            
             }
             
 #if DEBUG_NORMALS
@@ -316,9 +298,9 @@ namespace cbpipe {
 //            Diagnostics::SaveDiagnosticImage(0, markers, "markers-after");
 
             //watershed
-            ImageProcessing::refineMask(nonFloorMask, rgb, 20, 20, nonFloorMask);
-            nonWallMask.setTo(0, nonFloorMask > 0);
-        }
+            //ImageProcessing::refineMask(nonFloorMask, rgb, 20, 20, nonFloorMask);
+            //nonWallMask.setTo(0, nonFloorMask > 0);
+        //}
     };
     
     CBP_SemanticAnalyzer::CBP_SemanticAnalyzer() : CBP_AreaAnalyzer("CBP_SemanticAnalyzer") {
