0c6ca89a1771c40964b438a048570a179adcdec2
Joel Teply <joel@cambriantech.com>
2019-12-19 17:21:20 -0600

    uh ok

--- ios/FloorAR/FloorAR/FloorAR/Screens/CatalogViewController.swift
+++ ios/FloorAR/FloorAR/FloorAR/Screens/CatalogViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CatalogRow: UICollectionViewCell {
+class CatalogProductCell: UICollectionViewCell {
     @IBOutlet weak var label: UILabel!
     
     func initialize() {
@@ -26,17 +26,63 @@ class CatalogRow: UICollectionViewCell {
         initialize()
     }
     
-    private var _selectedCategory:ProductCategory?
-    var selectedCategory:ProductCategory? {
+    private var _selectedColor:ProductColor?
+    var selectedProduct:ProductColor? {
         get {
-            if let category = _selectedCategory {
-                return category
+            if let color = _selectedColor {
+                return color
             }
             return nil
         }
         set {
-            _selectedCategory = newValue
+            _selectedColor = newValue
         }
+    }
+}
+
+class CatalogRow: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var productLabel: UILabel!
+    
+    func initialize() {
+        self.isOpaque = false
+        self.backgroundColor = .clear
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+    
+    private var _selectedProduct:Product?
+    var selectedProduct:Product? {
+        get {
+            if let product = _selectedProduct {
+                return product
+            }
+            return nil
+        }
+        set {
+            _selectedProduct = newValue
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CatalogProductCell", for: indexPath) as? CatalogProductCell else {
+            fatalError("cannot find CatalogRow")
+        }
+        
+        productLabel.text = selectedProduct?.name
+        
+        return cell
     }
 }
 
@@ -64,11 +110,11 @@ class CatalogViewController: UIViewController, UICollectionViewDelegate, UIColle
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CatalogRow", for: indexPath) as? CatalogRow, let categories = selectedCategory?.categories else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CatalogRow", for: indexPath) as? CatalogRow, let products = selectedCategory?.products else {
             fatalError("cannot find CatalogRow")
         }
         
-        cell.selectedCategory = categories[indexPath.row]
+        cell.selectedProduct = products[indexPath.row]
         
         return cell
     }
