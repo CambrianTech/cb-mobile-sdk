89d70330535a7539d9128fe0e8e0b651eb937a92
Joel Teply <joel@cambriantech.com>
2019-12-23 14:07:25 -0600

    set app height, etc

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie9'
 import 'react-app-polyfill/stable'
 import cssVars from 'css-vars-ponyfill'
 
-import React, {useReducer, useEffect, useCallback} from "react"
+import React, {useReducer, useEffect, useCallback, useState} from "react"
 import * as ReactDOM from "react-dom"
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
@@ -15,11 +15,8 @@ const objectFitImages = require('object-fit-images')
 function App() {
     const initialSiteState = createEmptyState()
     const [siteState, dispatchSiteState] = useReducer(siteStateReducer, initialSiteState)
+    const [browserProperties, setBrowserProperties] = useState<BrowserProperties>({})
     // Url load states
-
-    const setBrowserProperties = useCallback((props:BrowserProperties)=>{
-        dispatchSiteState({ type: "setBrowserProperties", browserProperties: props })
-    }, [])
 
     //component mounted:
     useEffect(() => {
@@ -31,6 +28,38 @@ function App() {
         }
     }, [])
 
+    useEffect(() => {
+        if (browserProperties.hasTouchpad) {
+            document.documentElement.style.setProperty("--scrollbar-style", "none")
+            document.documentElement.style.setProperty("--scrollbar-display", "none")
+            document.documentElement.style.setProperty("--scrollbar-thickness", "0px")
+        }
+    }, [browserProperties.hasTouchpad])
+
+    const setCssVars = useCallback(() => {
+        if (!browserProperties.browser) return;
+
+        const doc = document.documentElement;
+
+        //without this check, causes WebGL flicker on desktop
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        doc.style.setProperty("--app-height",  `${height}px`);
+        doc.style.setProperty("--inverse-app-height", `${-height}px`);
+        doc.style.setProperty("--half-app-height", `${height / 2}px`);
+        doc.style.setProperty("--inverse-half-app-height", `${-height / 2}px`);
+        doc.style.setProperty("--app-width", `${width}px`);
+        doc.style.setProperty("--inverse-app-width", `${-width}px`)
+
+    }, [browserProperties.browser])
+
+    const _setBrowserProperties = useCallback((props:BrowserProperties)=>{
+        setBrowserProperties(props)
+        dispatchSiteState({ type: "setBrowserProperties", browserProperties: props })
+        setCssVars()
+    }, [setCssVars, dispatchSiteState])
+
     return (
         <Router>
             <Route
@@ -39,7 +68,7 @@ function App() {
                     return (
                         <SiteContext.Provider value={{ state: siteState, dispatch: dispatchSiteState }}>
 
-                            <WebClientInfo onClientStateChanged={setBrowserProperties} />
+                            <WebClientInfo onClientStateChanged={_setBrowserProperties} />
 
                             <Switch location={location}>
                                 <Route exact path="/" component={Visualizer} />
