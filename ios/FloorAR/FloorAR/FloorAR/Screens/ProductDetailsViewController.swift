fd624b51a1b0afff0d2e0b9fae1e382bdf2df0cb
Joel Teply <joel@cambriantech.com>
2019-12-27 21:25:31 -0600

    working callback

--- ios/FloorAR/FloorAR/FloorAR/Screens/ProductDetailsViewController.swift
+++ ios/FloorAR/FloorAR/FloorAR/Screens/ProductDetailsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ProductDetailsViewController: UIViewController, ProductSelectionDelegate, WKUIDelegate, WKNavigationDelegate {
+class ProductDetailsViewController: UIViewController, ProductSelectionDelegate, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
     @IBOutlet weak var webview: WKWebView!
     
     @IBAction func closeClicked(_ sender: Any) {
@@ -31,19 +31,34 @@ class ProductDetailsViewController: UIViewController, ProductSelectionDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        let script = """
+                    function webviewLoaded() {
+                        window.setTimeout(function() {
+                            webkit.messageHandlers.callbackHandler.postMessage({'command':'loaded'})
+                        }, 500)
+                    }
+                    if (window.attachEvent) {window.attachEvent('onload', webviewLoaded);}
+                    else if (window.addEventListener) {window.addEventListener('load', webviewLoaded, false);}
+                    else {document.addEventListener('load', webviewLoaded, false);}
+                    """
+        let userScript = WKUserScript(source: script, injectionTime: .atDocumentStart, forMainFrameOnly: true)
         let link = URL(string:"https://mobile.cambrianar.com/details")!
         let request = URLRequest(url: link, cachePolicy:flushCache ? .reloadIgnoringLocalAndRemoteCacheData : .useProtocolCachePolicy)
         webview.uiDelegate = self
         webview.navigationDelegate = self
         webview.configuration.preferences.javaScriptEnabled = true
+        webview.configuration.userContentController.add(self, name: "callbackHandler")
+        webview.configuration.userContentController.addUserScript(userScript)
         webview.load(request)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        if let product = self.product, let color = self.color {
-            productColorChanged(product: product, color: color)
-        }
+        
     }
     
     func productColorChanged(product: Product, color: ProductColor) {
@@ -59,6 +74,25 @@ class ProductDetailsViewController: UIViewController, ProductSelectionDelegate, 
                 print("Command error")
             }
         })
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: () -> Void) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true)
+        
+        completionHandler()
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        let body = message.body
+        if let dict = body as? Dictionary<String, AnyObject> {
+            if let command = dict["command"] as? String {
+                if command == "loaded", let product = self.product, let color = self.color {
+                    productColorChanged(product: product, color: color)
+                }
+            }
+        }
     }
 }
 
