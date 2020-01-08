4095991a4a8e291b15505dc627aaf18de97c505b
Joel Teply <joel@cambriantech.com>
2020-01-08 11:51:12 -0600

    do not store thumbnailPath, calculate it

--- ios/FloorAR/FloorAR/FloorAR/DB/ProductColor+Extended.swift
+++ ios/FloorAR/FloorAR/FloorAR/DB/ProductColor+Extended.swift
@@ -16,7 +16,7 @@ extension ProductColor {
         self.jsonString = json.jsonString
         self.code = json["UniqueId"] as! String
         self.name = json["SellingColorName"] as! String
-        self.thumbnailPath = DataSource.getThumbnailPath(self.code)
+        //self.thumbnailPath = DataSource.getThumbnailPath(self.code)
     }
     
     var thumbnailUrl : URL? {
