4f32cf2ab176bd2526f4af5957f14729ccb68e55
Joel Teply <joel@cambriantech.com>
2020-01-22 13:36:53 -0600

    new vc

---
+++ ios/FloorAR/FloorAR/FloorAR/Screens/PaintARViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PaintARViewController.swift
+//  DesignUp
+//
+//  Created by Joel Teply on 1/22/20.
+//  Copyright © 2020 Joel Teply. All rights reserved.
+//
+
+import UIKit
+
+class PaintARViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
