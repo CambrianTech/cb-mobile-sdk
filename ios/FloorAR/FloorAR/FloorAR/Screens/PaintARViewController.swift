921b2f9b88f926448ab1484806c7c8a7ccd36203
Joel Teply <joel@cambriantech.com>
2020-01-23 13:53:03 -0600

    kinda

--- ios/FloorAR/FloorAR/FloorAR/Screens/PaintARViewController.swift
+++ ios/FloorAR/FloorAR/FloorAR/Screens/PaintARViewController.swift
@@ -13,11 +13,15 @@ import AVFoundation
 class PaintARViewController: UIViewController, CBARRemodelingViewDelegate {
     
     @IBOutlet weak var arView: CBARRemodelingView!
+    var paint = CBARRemodelingPaint(assetID: "Paint")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.arView.delegate = self;
+        self.arView.isAREnabled = true
+        self.paint.color = UIColor.blue
+        self.arView.toolMode = .fill
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -36,6 +40,13 @@ class PaintARViewController: UIViewController, CBARRemodelingViewDelegate {
 
     func startRunning() {
         self.arView.startRunning()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.arView.scene.appendAsset(self.paint)
+        }
+    }
+    
+    func toolVisibilityChanged(visible:Bool) {
+        
     }
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
