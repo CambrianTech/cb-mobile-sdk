7fcb19937f2babb916ff2660d6ada61313a25cdc
joelteply <joelteply@yahoo.com>
2022-11-23 22:24:58 -0600

    new scripts

--- unity/CambrianUnity/transfer-code.sh
+++ unity/CambrianUnity/transfer-code.sh
@@ -11,13 +11,15 @@ SRC_PROJECT_ROOT="${CBROOT}/unity/Build/ios"
 DEST_PROJECT_ROOT="${CBROOT}/unity/CambrianUnity/Unity"
 
 #TRANSFER NATIVES
-NATIVE_SRC="${SRC_PROJECT_ROOT}/Classes/Native"
-NATIVE_DEST="${DEST_PROJECT_ROOT}/Classes/Native"
+NATIVE_SRC="${SRC_PROJECT_ROOT}/Classes"
+NATIVE_DEST="${DEST_PROJECT_ROOT}/Classes"
+
 
 rm -Rf ${NATIVE_DEST}
-cp -R ${NATIVE_SRC} "${DEST_PROJECT_ROOT}/Classes"
+cp -R ${NATIVE_SRC} "${DEST_PROJECT_ROOT}"
 touch ${NATIVE_DEST}/*.cpp
 #rm ${NATIVE_DEST}/Generated*
+
 
 #TRANSFER libcpp
 LIBS_SRC="${SRC_PROJECT_ROOT}/Libraries"
@@ -28,4 +30,6 @@ rm -Rf ${LIBS_DEST}/libil2cpp
 
 cp -R ${LIBS_SRC}/libil2cpp ${LIBS_DEST}
 
-cp -Rf ${LIBS_SRC}/* ${LIBS_DEST}/
+#cp -Rf ${LIBS_SRC}/*.* ${LIBS_DEST}
+
+rsync -av --progress ${LIBS_SRC} ${LIBS_DEST} --exclude CambrianARPlugin
